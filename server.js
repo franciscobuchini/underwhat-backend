71dae86ed46a6fc2365d12ea264b63858d472c85
franciscobuchini <franciscobuchini@gmail.com>
2025-04-06 15:05:48 -0300

    Remover node_modules del repositorio

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
-//server.js
+// server.js
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose(); // Importar sqlite3
+const Database = require('better-sqlite3'); // Cambiado a better-sqlite3
 const path = require('path');
 const app = express();
 const cors = require('cors');
-const corsOptions = { origin: 'http://localhost:5174', };
 
+const corsOptions = { origin: 'http://localhost:5174' };
 app.use(cors(corsOptions));
 
 // Conectar a la base de datos
 const dbPath = path.resolve(__dirname, './database/underwhat.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error('Error al conectar con la base de datos:', err.message);
-  } else {
-    // console.log('Conexión exitosa a la base de datos SQLite');
-  }
-});
-
+let db;
+try {
+  db = new Database(dbPath); // conexión sin callbacks
+  // console.log('Conexión exitosa a la base de datos SQLite');
+} catch (err) {
+  console.error('Error al conectar con la base de datos:', err.message);
+}
 
 // Ruta para obtener datos desde la base de datos
 app.get('/api', (req, res) => {
-  const query = 'SELECT product_name, product_selling, product_category, product_id, product_image, product_image02 FROM Products';
-  db.all(query, [], (err, rows) => {
-    if (err) {
-      console.error('Error ejecutando la consulta:', err.message);
-      res.status(500).json({ error: 'Error ejecutando la consulta' });
-    } else {
-      res.json({ products: rows });
-    }
-  });
+  try {
+    const query = `
+      SELECT product_name, product_selling, product_category, product_id, product_image, product_image02
+      FROM Products
+    `;
+    const stmt = db.prepare(query);
+    const products = stmt.all(); // no necesita callback
+    res.json({ products });
+  } catch (err) {
+    console.error('Error ejecutando la consulta:', err.message);
+    res.status(500).json({ error: 'Error ejecutando la consulta' });
+  }
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3002; // Si no hay PORT definido, usa 3002
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
